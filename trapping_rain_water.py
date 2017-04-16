1cdeddbeff4844bf9cb15f04d3f0785f51ee60c0
jungker <seek_zp@163.com>
2017-04-16 20:30:29 +0800

    commit 1

---
+++ trapping_rain_water.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+
+
+if __name__ == '__main__':
+    x = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    solution = Solution()
+    print solution.trap(x)
