1bb11a6b3aef7c6017b29c77c4982fb57a8be493
jungker <seek_zp@163.com>
2017-04-16 20:56:22 +0800

    commit 2 4/16

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -4,7 +4,20 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
+        size = len(height)
+        if not size:
+            return 0
+        leftMax = [0 for i in range(size)]
+        rightMax = [0 for i in range(size)]
+        for i in range(1, size):
+            leftMax[i] = max(leftMax[i - 1], height[i - 1])
+            rightMax[size - i - 1] = max(rightMax[size - i], height[size - i])
+        res = 0
+        for i in range(1, size - 1):
+            t = min(leftMax[i], rightMax[i])
+            if t > height[i]:
+                res += (t - height[i])
+        return res
 
 
 if __name__ == '__main__':
